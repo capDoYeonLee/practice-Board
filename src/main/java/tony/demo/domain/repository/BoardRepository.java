333a482099781258263594fd67778ab70d000735
capDoYeonLee <ghtyu21@naver.com>
2023-11-14 14:23:32 +0900

    remove:파일 삭제 및 경로 수정

--- src/main/java/tony/demo/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package tony.demo.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tony.demo.domain.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
